e4bbd21e92bd4ca72a8f23ee8b0a069db50a4083
niktar <niktar-sch@bk.ru>
2020-07-09 10:38:30 +0300

    c3-9

--- js/index.js
+++ js/index.js
@@ -1,9 +1,52 @@
+// 1. Ваш город — Владивосток?
+//
 const cityInp = document.getElementById('city');
 
-console.log(document.coockie);
-console.log(decodeURIComponent(document.coockie));
+// возвращает куки с указанным name,
+// или undefined, если ничего не найдено      
+ function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+ return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 
-cityInp.addEventListener('input', (e)  => {
-	document.coockie = `city=${encodeURIComponent(e.target.value)}; Max-Age=3600`;
-	console.log(e.target.value);
+qky = getCookie('city');
+
+if (qky) {
+	document.getElementById('cityform').outerHTML = `<p>Ваш город - ${qky}</p><a href="" id="del_city">Удалить запись</a>`
+	document.getElementById('del_city').addEventListener('click', (e)  => {
+		document.cookie = `city=${encodeURIComponent(qky)}; max-age=0`;
+	});
+
+}
+
+cityInp.addEventListener('input', (e)  => {document.cookie = `city=${encodeURIComponent(e.target.value)}; max-age=3600`});
+
+
+// 2. Галочки предпочтений
+//
+const btnSave = document.getElementById('save');
+
+const checksInps = document.querySelectorAll('input[type="checkbox"]');
+
+const checksJSON = localStorage.getItem('checks');
+// Отключаем кнопку "Сохранить", если данные были сохранены
+if (checksJSON) {
+	btnSave.disabled = true;
+	const checksDict = JSON.parse(checksJSON);
+	for (let check of checksInps) {
+		check.checked = checksDict[check.id];
+	}
+}
+
+btnSave.addEventListener('click', () => {
+	const checksDict = {};
+	for (let check of checksInps) {
+		checksDict[check.id] = check.checked;
+	}
+	const checksJ = JSON.stringify(checksDict);
+
+	localStorage.setItem('checks', checksJ);
+	console.log(localStorage.getItem('checks'));
 });
